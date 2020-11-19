6574a26c76498428df7c151567f5e0701888f551
Dilshad <dilshad.alam3215@gmail.com>
2020-11-19 12:09:33 +0545

    front-end changed

--- script.js
+++ script.js
@@ -1,29 +1,19 @@
-const videoElement = document.querySelector('.video');
-const btn1 = document.querySelector('.btn-1')
-const btn2 = document.querySelector('.btn-2')
+const videoElement = document.querySelector(".video");
+const btn1 = document.querySelector(".btn-1");
+const btn2 = document.querySelector(".btn-2");
+const container = document.querySelector(".container");
 
-
-btn2.disabled = true;
-
-async function selectMediaStream(){
-    try{
-        const captureStream = await navigator.mediaDevices.getDisplayMedia();
-        videoElement.srcObject = captureStream;
-        videoElement.onloadedmetadata = ()=>{
-        videoElement.play();
-        btn2.disabled = false;
-
-        }
-    }catch(err){
-        console.log(err)
-    }
+async function selectMediaStream() {
+  try {
+    const captureStream = await navigator.mediaDevices.getDisplayMedia();
+    videoElement.srcObject = captureStream;
+    videoElement.onloadedmetadata = () => {
+      videoElement.play();
+      videoElement.requestPictureInPicture();
+      console.log("success");
+    };
+  } catch (err) {
+    console.log(err);
+  }
 }
-
-btn1.addEventListener('click',selectMediaStream);
-
-btn2.addEventListener('click',  ()=>{
-     videoElement.requestPictureInPicture();
-})
-
-
-
+btn1.addEventListener("click", selectMediaStream);
