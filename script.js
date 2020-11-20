08a1d6d75049b602e50a097fc326c728c97650b4
Dilshad <dilshad.alam3215@gmail.com>
2020-11-20 10:33:59 +0545

    final commit

--- script.js
+++ script.js
@@ -3,17 +3,26 @@ const btn1 = document.querySelector(".btn-1");
 const btn2 = document.querySelector(".btn-2");
 const container = document.querySelector(".container");
 
+
+
 async function selectMediaStream() {
   try {
+    btn2.disabled = false;
     const captureStream = await navigator.mediaDevices.getDisplayMedia();
     videoElement.srcObject = captureStream;
-    videoElement.onloadedmetadata = () => {
+    videoElement.onloadedmetadata = setTimeout(() => {
       videoElement.play();
-      videoElement.requestPictureInPicture();
-      console.log("success");
-    };
+    },2000);
   } catch (err) {
     console.log(err);
   }
 }
+
+function reqpic(){
+  videoElement.requestPictureInPicture();
+}
+
 btn1.addEventListener("click", selectMediaStream);
+btn2.addEventListener("click",reqpic)
+
+
